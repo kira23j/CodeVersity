5b3441549352a97ba637dce216fce5522ff2b54d
kira23j <kirubelmamuyesahle@gmail.com>
2024-02-26 10:30:47 -0800

    reorganizing and adding parts on dart section

---
+++ 04.Dart/02.E2E-Dart/01.Variables.dart
@@ -0,0 +1,23 @@
+void main() {
+  // variables in dart
+  int firstValue = 23;
+  int secoundValue = 32;
+  print(firstValue + secoundValue);
+  String One = "hello";
+  String Two = "World";
+  print(One + Two);
+  dynamic someValue = 23;
+  print(someValue);
+  // another way of creating variables
+  var otherValue = 10;
+  print(otherValue);
+  // final run time constant
+  final otherValue2 = 10;
+  print(otherValue2);
+  // const compile time constant
+  const otherValue3 = '10';
+  print(otherValue3);
+  // to assign null value
+  int? value1 = null;
+  print(value1);
+}
---
+++ 04.Dart/02.E2E-Dart/02.Conditional.dart
@@ -0,0 +1,9 @@
+void main() {
+  // if condition
+  int age = 21;
+  if (age > 10) {
+    print("qualified");
+  } else {
+    print("not qualified");
+  }
+}
