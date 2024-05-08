c3b84791c422befebc0d9f512a502f330dda1f2d
kira23j <kirubelmamuyesahle@gmail.com>
2024-05-08 12:55:07 -0700

    updating the repo

---
+++ 03.Typescript/01.Fundamentals/index.ts
@@ -0,0 +1,3 @@
+console.log('Hello World');
+console.log("Hello ad Welcome");
+console.log("Welcome, Welcome")
--- 07.Web3-Camp/01.Solidity/Hello.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-contract HelloWorld {
-    // Function to return a greeting message
-    function sayHello() public pure returns (string memory) {
-        return "Hello, World!";
-    }
-}
