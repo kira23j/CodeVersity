8742c1f76b03ccabd796577c273fc0e2e7850032
kira23j <kirubelmamuyesahle@gmail.com>
2024-09-15 14:07:30 -0700

    adding other ds section on mpython

--- 01.Python/Revision_with_Mosh/4-data-structures/other_ds.py
+++ 01.Python/Revision_with_Mosh/4-data-structures/other_ds.py
@@ -14,4 +14,58 @@ if not queue:
 # tuple
 point = (1, 2, 3)
 print(type(point))
+# swap
+x = 10
+y = 11
+x, y = y, x
+print(x, y)
 
+
+# array for performance
+from array import array
+
+numbers = array("i", [1, 2, 3])
+# numbers[0] = 1.0
+
+# set 
+
+numbers = [1, 2, 3, 4, ]
+first = set(numbers)
+second = {1, 4}
+second.add(5)
+second.remove(1)
+
+union = first | second
+intersection = first & second
+difference = first - second
+symmetric_difference = (first ^ second)
+
+print(union, intersection, difference, symmetric_difference)
+
+# dictionaries
+
+point_one = {"x": 1, "y": 2 }
+point_two = dict(x=1, y=2)
+point_one["x"] = 3
+print(point_one, point_two)
+if x in point:
+    print(point_two["x"])
+print(point_two.get("x", 0))
+del point_one["x"]
+for key, value in point_two.items():
+    print(key, value)
+    
+# dic comprehension
+values = {}
+values = {x: x * 2 for x in range(5)}
+print(values)
+
+# unpacking operator
+first = {"a": 1, "b": 2, "c": 3}
+second= {"d": 1, "e": 2, "f": 3}
+combined = {**first, **second, "z": 1}
+print(combined)
+
+# zip                                
+print(list(zip("abc", [1, 2, 3])))  
+                                                        
